c5d314c67989854b6b81b77f3ea47e2f96e86bdd
Dez <desmondadkins@gmail.com>
2021-04-27 17:25:12 -0400

    add reminders done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks
+      v-on:toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      v-bind:tasks="tasks"
+    />
   </div>
 </template>
 
@@ -24,6 +28,12 @@ export default {
       if (confirm("Are you sure?")) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
       }
+    },
+    toggleReminder(id) {
+      // console.log(id);
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
     },
   },
   //LifeCycle method  (created())
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,7 +1,7 @@
 <template>
   <div :key="task.id" v-for="task in tasks">
     <Task
-      @toggle-reminder="$emit('toggle-reminder, task.id')"
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
       @delete-task="$emit('delete-task', task.id)"
       :task="task"
     />
