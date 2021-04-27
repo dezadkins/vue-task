1ad4528b3fada42c4e5b896aba07b19d62cdd762
Dez <desmondadkins@gmail.com>
2021-04-27 16:50:18 -0400

    emitting on delete through components.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
       tasks: [],
     };
   },
+  methods: {
+    deleteTask(id) {
+      console.log('task', id)
+    }
+  }
   //LifeCycle method  (created())
   created() {
     this.tasks = [
