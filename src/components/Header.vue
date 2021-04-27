b9b419b39c122299ea994d709faafb1979167bf1
Dez <desmondadkins@gmail.com>
2021-04-27 12:13:17 -0400

    button component with styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,30 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
   </header>
 </template>
 
 <script>
+import Button from "./Button";
 export default {
   name: "Header",
-  props: ["title"],
+  // Could do this ......
+  //   props: ["title"],
+  // OR //
+  //   props: {
+  //     title: String,
+  //   },
+  // OR //
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+  },
+  components: {
+    Button,
+  },
 };
 </script>
 
