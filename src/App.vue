b1a182d5f1f2044ac6a455440b9a707823bad469
Dez <desmondadkins@gmail.com>
2021-04-28 09:09:37 -0400

    add task created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <AddTask />
+    <AddTask @add-task="addTask" />
     <Tasks
       v-on:toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
@@ -28,6 +28,9 @@ export default {
     };
   },
   methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
     deleteTask(id) {
       if (confirm("Are you sure?")) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,16 +1,22 @@
 <template>
-  <form class="add-form">
+  <form @submit="onSubmit" class="add-form">
     <div class="form-control">
       <label>Task</label>
-      <input type="text" placeholder="Add Task" />
+      <input type="text" name="text" v-model="text" placeholder="Add Task" />
     </div>
+
     <div class="form-control">
-      <label>Day & Time</label>
-      <input type="text" name="day" placeholder="Add Day & Time" />
+      <label>Day & Time </label>
+      <input
+        type="text"
+        name="day"
+        v-model="day"
+        placeholder="Add Day & Time "
+      />
     </div>
     <div class="form-control form-control-check">
       <label>Set Reminder</label>
-      <input type="checkbox" name="reminder" />
+      <input type="checkbox" name="reminder" v-model="reminder" />
     </div>
     <input type="submit" value="Save Task" class="btn btn-block" />
   </form>
@@ -19,6 +25,35 @@
 <script>
 export default {
   name: "AddTask",
+  data() {
+    return {
+      text: "",
+      day: "",
+      reminder: false,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      if (!this.text) {
+        alert("Please add a task");
+        return;
+      }
+      //since we don't have a back end.. do newTask below
+      const newTask = {
+        id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
+
+      this.$emit("add-task", newTask);
+      // Below clears the form
+      this.text = "";
+      this.day = "";
+      this.reminder = false;
+    },
+  },
 };
 </script>
 
