57c29084fa9aecc6c3a4be574790ba0a11c7b6b6
Dez <desmondadkins@gmail.com>
2021-04-28 10:13:06 -0400

    add task button boolean

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,8 @@
     <h1>{{ title }}</h1>
     <Button
       @toggle-add-task="$emit('toggle-add-task')"
-      text="Add Task"
-      color="green"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
     />
   </header>
 </template>
@@ -23,8 +23,9 @@ export default {
   props: {
     title: {
       type: String,
-      default: "Hello World",
+      default: "Dez Tracker",
     },
+    showAddTask: Boolean,
   },
   components: {
     Button,
